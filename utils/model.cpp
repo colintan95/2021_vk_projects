6d88932d1e7f17a5d41f2ec85e68d37e2e24e877
Colin Tan <colintan95@gmail.com>
2021-10-04 08:52:25 -0700

    Added normals generation

--- utils/model.cpp
+++ utils/model.cpp
@@ -24,7 +24,11 @@ bool LoadObjFile(const std::string& path, Model* out_model,
   std::string line;
   std::string token;
 
-  std::vector<int> face_verts;
+  std::vector<glm::vec3> positions;
+  std::vector<int> face_pos_indices;
+  std::vector<int> face_normal_indices;
+
+  uint16_t current_idx = 0;
 
   while (std::getline(file_strm, line)) {
     std::stringstream sstrm(line);
@@ -40,34 +44,81 @@ bool LoadObjFile(const std::string& path, Model* out_model,
       } else if (token == "v") {
         float f1, f2, f3;
         sstrm >> f1 >> f2 >> f3;
-        model.positions.push_back({ f1, f2, f3 });
+        positions.push_back({ f1, f2, f3 });
 
       } else if (token == "f") {
-        face_verts.clear();
+        face_pos_indices.clear();
+        face_normal_indices.clear();
+
         int i;
         while (sstrm >> i)
-          face_verts.push_back(i);
+          face_pos_indices.push_back(i);
 
-        int top_idx = model.positions.size();
-        for (int i = 0; i < face_verts.size(); ++i) {
-          if (face_verts[i] < 0)
-            face_verts[i] = top_idx + face_verts[i];
+        int top_pos_idx = positions.size();
+        for (int i = 0; i < face_pos_indices.size(); ++i) {
+          if (face_pos_indices[i] < 0)
+            face_pos_indices[i] = top_pos_idx + face_pos_indices[i];
 
-          if (face_verts[i] < 0)
+          if (face_pos_indices[i] < 0)
             return false;
         }
 
-        if (face_verts.size() == 3) {
-          model.index_buffer.push_back(face_verts[0]);
-          model.index_buffer.push_back(face_verts[1]);
-          model.index_buffer.push_back(face_verts[2]);
-        } else if (face_verts.size() == 4) {
-          model.index_buffer.push_back(face_verts[0]);
-          model.index_buffer.push_back(face_verts[1]);
-          model.index_buffer.push_back(face_verts[2]);
-          model.index_buffer.push_back(face_verts[0]);
-          model.index_buffer.push_back(face_verts[2]);
-          model.index_buffer.push_back(face_verts[3]);
+        if (face_pos_indices.size() == 3) {
+          model.positions.push_back(positions[face_pos_indices[0]]);
+          model.positions.push_back(positions[face_pos_indices[1]]);
+          model.positions.push_back(positions[face_pos_indices[2]]);
+
+          if (face_normal_indices.empty()) {
+            glm::vec3 n1 = glm::cross(positions[face_pos_indices[1]] -
+                                          positions[face_pos_indices[0]],
+                                      positions[face_pos_indices[2]] -
+                                          positions[face_pos_indices[0]]);
+            n1 = glm::normalize(n1);
+            model.normals.push_back(n1);
+            model.normals.push_back(n1);
+            model.normals.push_back(n1);
+          }
+
+          model.index_buffer.push_back(current_idx);
+          model.index_buffer.push_back(current_idx + 1);
+          model.index_buffer.push_back(current_idx + 2);
+
+          current_idx += 3;
+        } else if (face_pos_indices.size() == 4) {
+          model.positions.push_back(positions[face_pos_indices[0]]);
+          model.positions.push_back(positions[face_pos_indices[1]]);
+          model.positions.push_back(positions[face_pos_indices[2]]);
+          model.positions.push_back(positions[face_pos_indices[0]]);
+          model.positions.push_back(positions[face_pos_indices[2]]);
+          model.positions.push_back(positions[face_pos_indices[3]]);
+
+          if (face_normal_indices.empty()) {
+            glm::vec3 n1 = glm::cross(positions[face_pos_indices[1]] -
+                                          positions[face_pos_indices[0]],
+                                      positions[face_pos_indices[2]] -
+                                          positions[face_pos_indices[0]]);
+            glm::vec3 n2 = glm::cross(positions[face_pos_indices[2]] -
+                                          positions[face_pos_indices[0]],
+                                      positions[face_pos_indices[3]] -
+                                          positions[face_pos_indices[0]]);
+            n1 = glm::normalize(n1);
+            n2 = glm::normalize(n2);
+            model.normals.push_back(n1);
+            model.normals.push_back(n1);
+            model.normals.push_back(n1);
+            model.normals.push_back(n2);
+            model.normals.push_back(n2);
+            model.normals.push_back(n2);
+          }
+
+          model.index_buffer.push_back(current_idx);
+          model.index_buffer.push_back(current_idx + 1);
+          model.index_buffer.push_back(current_idx + 2);
+          model.index_buffer.push_back(current_idx + 3);
+          model.index_buffer.push_back(current_idx + 4);
+          model.index_buffer.push_back(current_idx + 5);
+
+          current_idx += 6;
         } else {
           return false;
         }
