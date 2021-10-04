0832db3cbeb2010a1290df28619a18cf4b42aa64
Colin Tan <colintan95@gmail.com>
2021-10-03 19:26:42 -0700

    Upload vertex buffer

--- hello_triangle/app.cpp
+++ hello_triangle/app.cpp
@@ -295,7 +295,8 @@ VkShaderModule CreateShaderModule(const std::vector<char>& shader_data,
   VkShaderModuleCreateInfo shader_module_info = {};
   shader_module_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
   shader_module_info.codeSize = shader_data.size();
-  shader_module_info.pCode = reinterpret_cast<const uint32_t*>(shader_data.data());
+  shader_module_info.pCode =
+      reinterpret_cast<const uint32_t*>(shader_data.data());
 
   VkShaderModule shader_module;
   if (vkCreateShaderModule(device, &shader_module_info, nullptr, &shader_module)
@@ -357,16 +358,6 @@ bool CreateBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
 
   return true;
 }
-
-// void CopyBuffer(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
-//   VkCommandBuffer commandBuffer = beginSingleTimeCommands();
-
-//   VkBufferCopy copyRegion{};
-//   copyRegion.size = size;
-//   vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
-
-//   endSingleTimeCommands(commandBuffer);
-// }
 
 }  // namespace
 
@@ -808,6 +799,22 @@ bool App::Init() {
     return false;
   }
 
+  VkCommandBufferAllocateInfo init_command_buffer_info = {};
+  init_command_buffer_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  init_command_buffer_info.commandPool = command_pool_;
+  init_command_buffer_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  init_command_buffer_info.commandBufferCount = 1;
+
+  VkCommandBuffer init_command_buffer;
+  vkAllocateCommandBuffers(device_, &init_command_buffer_info,
+                           &init_command_buffer);
+
+  VkCommandBufferBeginInfo init_commands_begin_info = {};
+  init_commands_begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+  init_commands_begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+  vkBeginCommandBuffer(init_command_buffer, &init_commands_begin_info);
+
   std::vector<glm::vec2> vertex_position_data = {
     {-0.5f, -0.5f}, {0.5f, -0.5f}, {0.f, 0.5f}
   };
@@ -815,33 +822,46 @@ bool App::Init() {
   VkDeviceSize vertex_buffer_size =
       sizeof(glm::vec2) * vertex_position_data.size();
 
-  {
-    VkBuffer staging_buffer;
-    VkDeviceMemory staging_buffer_memory;
-    CreateBuffer(vertex_buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                     VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                 physical_device_, device_, staging_buffer,
-                 staging_buffer_memory);
+  VkBuffer vertex_staging_buffer;
+  VkDeviceMemory vertex_staging_buffer_memory;
+  CreateBuffer(vertex_buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+               VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                   VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+               physical_device_, device_, vertex_staging_buffer,
+               vertex_staging_buffer_memory);
 
-    void* data;
-    vkMapMemory(device_, staging_buffer_memory, 0, vertex_buffer_size, 0,
-                &data);
-    memcpy(data, vertex_position_data.data(),
-           static_cast<size_t>(vertex_buffer_size));
-    vkUnmapMemory(device_, staging_buffer_memory);
+  void* data;
+  vkMapMemory(device_, vertex_staging_buffer_memory, 0, vertex_buffer_size, 0,
+              &data);
+  memcpy(data, vertex_position_data.data(),
+          static_cast<size_t>(vertex_buffer_size));
+  vkUnmapMemory(device_, vertex_staging_buffer_memory);
 
-    CreateBuffer(vertex_buffer_size,
-                 VK_BUFFER_USAGE_TRANSFER_DST_BIT |
-                     VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-                 VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, physical_device_,
-                 device_, vertex_buffer_, vertex_buffer_memory_);
+  CreateBuffer(vertex_buffer_size,
+               VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+                   VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+               VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, physical_device_,
+               device_, vertex_buffer_, vertex_buffer_memory_);
 
-    // CopyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+  VkBufferCopy copy_info = {};
+  copy_info.size = vertex_buffer_size;
+  vkCmdCopyBuffer(init_command_buffer, vertex_staging_buffer, vertex_buffer_, 1,
+                  &copy_info);
 
-    vkDestroyBuffer(device_, staging_buffer, nullptr);
-    vkFreeMemory(device_, staging_buffer_memory, nullptr);
-  }
+  vkEndCommandBuffer(init_command_buffer);
+
+  VkSubmitInfo init_commands_submit_info = {};
+  init_commands_submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+  init_commands_submit_info.commandBufferCount = 1;
+  init_commands_submit_info.pCommandBuffers = &init_command_buffer;
+
+  vkQueueSubmit(graphics_queue_, 1, &init_commands_submit_info, VK_NULL_HANDLE);
+  vkQueueWaitIdle(graphics_queue_);
+
+  vkFreeCommandBuffers(device_, command_pool_, 1, &init_command_buffer);
+
+  vkDestroyBuffer(device_, vertex_staging_buffer, nullptr);
+  vkFreeMemory(device_, vertex_staging_buffer_memory, nullptr);
 
   return true;
 }
